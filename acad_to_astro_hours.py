d5d01d90a2e101a8e9912afa2404cdf38017e3c2
DimiPetrov86 <dimi.petrov86@gmail.com>
2025-02-17 16:18:59 +0200

    acad_to_astro_hours

---
+++ acad_to_astro_hours.py
@@ -0,0 +1,6 @@
+astro_hour=60
+acad_hour=40
+session_break=20
+session=4*acad_hour+session_break
+total_amount_of_sessions=64/4
+print("Total duration in astronomical hours:", total_amount_of_sessions*session/astro_hour)
